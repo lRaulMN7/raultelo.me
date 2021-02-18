dd69411c72223c3bc299ee0429eaf4486e929720
Rauññ <raultelo@gmail.com>
2021-02-18 23:02:08 +0100

    Layout

--- backend/src/controllers/WordsController.ts
+++ backend/src/controllers/WordsController.ts
@@ -3,6 +3,17 @@ import { Request, Response } from 'express'
 class WordsController{
 
         public sarita (req: Request, res: Response){
+
+            /* const readXlsxFile = require('read-excel-file/node');
+            readXlsxFile('src/priv_resources/Notebook.xlsx').then( (rows: Array<Array<String>>) => {
+                  rows.forEach(cell => {
+                        console.log(cell[2])
+                        console.log("-")
+                        console.log(cell[0])
+                  })
+            }); */
+
+            
             res.send({data: "2"});
       }
 }
